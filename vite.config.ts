aa7c3e1fc11e098d8147be8102fc3bd768c8e191
TvilumBoy <122982788+TvilumBoy@users.noreply.github.com>
2025-09-12 23:00:52 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -7,7 +8,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [react()],
+    plugins: [react(), tsconfigPaths()],
     define: {
       // Expose the API key to the client-side code.
       // Vite will replace `process.env.API_KEY` with the value of `VITE_GEMINI_API_KEY`
